94c99aa59720d6ef84e2bcf95300a137a03cef15
wonsangL <harryandws@gmail.com>
2018-05-10 19:10:04 +0900

    최준문

---
+++ 균일수/최준문.c
@@ -0,0 +1,63 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int getBase(int n);
+int Answer;
+
+int main(void)
+{
+	int T, test_case, N;
+	setbuf(stdout, NULL);
+    
+	scanf("%d", &T);
+	for (test_case = 0; test_case < T; test_case++)
+	{
+		scanf("%d", &N);
+		Answer = getBase(N);
+		
+		printf("Case #%d\n", test_case + 1);
+		printf("%d\n", Answer);
+
+	}
+
+	return 0;//Your program should return 0 on normal termination.
+}
+
+int getBase(int Num)
+{
+	int Ans = 0,b = 2;
+	int Share = 0, Rest = 0;
+	bool check = true;
+
+	//if (Num % 2 == 0)	b = 3;
+	
+		for (int x = b; x <= N; x++)
+		{
+			Share = Num / x;
+			Rest = Num % x;
+			
+			while (1)
+			{
+				if (Share == 0) break;
+				if (Rest != Share % x) 
+				{
+					check = false; 
+					break;
+				}
+				else
+				{
+					check = true;
+				}
+				Rest = Share % x;
+				Share = Share / x;
+			}
+			
+			if (check == true) 
+			{
+				Ans = x;
+				break;
+			}
+		}
+
+	return Ans;
+}
