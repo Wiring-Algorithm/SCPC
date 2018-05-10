aefe9dd298b869a76c1f6f46888badad094b5fd9
yeahyung <35248661+yeahyung@users.noreply.github.com>
2018-05-10 09:59:14 +0900

    Add files via upload

---
+++ 균일수/예형빈.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void) {
+	int t, N;
+	int solve(int);
+	setbuf(stdout, NULL);
+	scanf("%d", &t);
+	for (int test_case = 0; test_case < t; test_case++) {
+		scanf("%d", &N);
+		printf("Case #%d\n", test_case + 1);
+		printf("%d\n", solve(N));
+	}
+	return 0;
+}
+
+int solve(int N) {
+	int digit2=N, digit3=N,b,basis,answer;
+/*
+    for(basis=2;basis<=N;basis++){
+        long temp=basis;
+        for(i=2;i<=N;i++){
+            if(temp>N)
+            break;
+            	if (N % (int)((temp*basis - 1) / (basis - 1)) == 0 && N / (int)((temp*basis - 1) / (basis - 1))<basis) {
+    			    return N;
+				break;
+            }
+        }
+    }   --> for(basis<=N) -> overflow
+        N = 1000000007인 경우 모든 for문을 도는데 if문에 걸리지 않음 -> time limit
+        for문의 N을 어떻게 줄일까?
+        3자리 수 이상의 경우 b^2 + b^1 + b^0 --> b^2 b가 루트N이 넘으면 N보다 크므로
+        루트N까지의 범위로 바꾸고 계산하여 답이 없는 경우에만
+        2자리 경우를 따로 계산해주면 됨(자리수가 클수록 b가 작기 때문에)
+*/
+	// i==3이상인 경우 N = (b^i + b^i-1 + .... + b^1 + b^0)k
+	for (basis = 2; basis <= (int)sqrt(N); basis++) {
+		long temp = basis*basis;
+		for (int i = 3; i <= (int)sqrt(N); i++) {
+			if (temp>N)
+				break;
+			if (N % (int)((temp*basis - 1) / (basis - 1)) == 0 && N / (int)((temp*basis - 1) / (basis - 1))<basis) {
+				digit3 = basis;
+				basis = N;
+				break;
+			}
+			temp *= basis;
+		}
+	}
+	if (basis == (int)sqrt(N)+1) {     // if문 통과 x -> 답 x
+		digit3 = N - 1;
+	
+	// i==2인 경우 N = (b^1 + b^0)k       k가 클수록 b값 감소
+	for (int k = (int)sqrt(N); k >= 2; k--) {
+		if (N%k == 0) {
+			b = N / k - 1;
+			if (b > k) {
+				digit2 = b;
+				break;
+			}
+		}
+	}
+	}
+	if(digit2<digit3)
+	digit3=digit2;
+
+    //모두 k>=2, i>=3이므로  N=1,2,3의 경우 for문을 돌아가지 않으므로 따로 답을 설정
+	if (N == 1) digit3 = 2;
+	if (N == 2) digit3 = 3;
+	if (N == 3) digit3 = 2;
+
+	return digit3;
+}
